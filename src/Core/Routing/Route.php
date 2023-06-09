24fdff14e31ca11964529b8e28047c8275051584
atheo810 <atheos810@gmail.com>
2023-06-09 21:09:37 +0800

    feat: Routing

---
+++ src/Core/Routing/Route.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Indoframe\Core\Routing;
+
+class Route
+{
+    private $method;
+    private $path;
+    private $controller;
+    private $action;
+
+    /**
+     * @param string $method
+     * @param string $path
+     * @param string $controller
+     * @param string $action
+     */
+    public function __construct(string $method, string $path, string $controller, string $action)
+    {
+        $this->method = $method;
+        $this->path = $path;
+        $this->controller = $controller;
+        $this->action = $action;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    public function getAction()
+    {
+        return $this->action;
+    }
+}
---
+++ src/Core/Routing/Router.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Indoframe\Core\Routing;
+
+class Router
+{
+    private $routes = [];
+
+    public function addRoute(Route $route)
+    {
+        $this->routes[] = $route;
+    }
+
+    /**
+     * @param string $path
+     */
+    public function get(string $path, $controller, $action)
+    {
+        $this->routes[] = new Route('GET', $path, $controller, $action);
+    }
+
+    private function match($method, $uri)
+    {
+        foreach ($this->routes as $route) {
+            $routeMethod = $route->getMethod();
+            $routePath = $route->getPath();
+
+            // Match HTTP method
+            if ($routeMethod !== $method) {
+                continue;
+            }
+
+            // Match URI using regex pattern
+            $pattern = '#^' . $routePath . '$#';
+            if (preg_match($pattern, $uri)) {
+                return $route;
+            }
+        }
+
+        return null;
+    }
+
+    public function run()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $method = $_SERVER['REQUEST_METHOD'];
+        $matchedRoute = $this->match($method, $uri);
+
+        if ($matchedRoute) {
+            $controllerName = $matchedRoute->getController();
+            $actionName = $matchedRoute->getAction();
+
+            // Check if controller class exists
+            $controllerClass = 'Indoframe\Controllers\\' . $controllerName;
+
+            if (class_exists($controllerClass)) {
+                $controller = new $controllerClass();
+
+                // Check if action method exists within the controller class
+                if (method_exists($controller, $actionName)) {
+                    $response = $controller->$actionName();
+
+                    // Use the response to display or send output
+                    echo $response;
+                    return;
+                }
+            }
+        }
+
+        // If no matching route or controller/action does not exist, display a 404 Not Found message
+        echo "404 Not Found";
+    }
+}
